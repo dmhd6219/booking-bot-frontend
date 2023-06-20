246472c23ed999d9bc9d8467cd3eb89236025d58
Iceflue2 <svyatoslavsvyatkin@yandex.ru>
2023-06-20 23:33:03 +0300

    added disabled values to starttime

--- src/components/sortings/ByStartTime.jsx
+++ src/components/sortings/ByStartTime.jsx
@@ -31,43 +31,33 @@ function ByStartTime() {
     return (
         <div id={"sort-by-start-time"}>
             <Typography.Title>Select Date</Typography.Title>
-            <WidthSelect defaultValue={"default"} onChange={(value) => {
-                console.log(value);
-                setDate(!(value === "default"))
-            }} size={"large"}>
+            <WidthSelect onChange={
+                value => console.log(value)} size={"large"} onSelect={() => setDate(true)}>
                 <Select.Option value="20.06.2022">20.06.2022</Select.Option>
                 <Select.Option value="21.06.2022">21.06.2022</Select.Option>
                 <Select.Option value="22.06.2022">22.06.2022</Select.Option>
             </WidthSelect>
 
             <Typography.Title>Select Time of Start</Typography.Title>
-            <TimePicker  onChange={(value) => {
-                console.log(value);
-                setTime(true)
-            }} inputReadOnly={true} disabledTime={disabledDateTime}
-                        format={"HH:mm"} minuteStep={5} size={"large"} disabled={!date}/>
+            <TimePicker onChange={(value) => console.log(value)} inputReadOnly={true}
+                        disabledTime={disabledDateTime} format={"HH:mm"}
+                        minuteStep={5} size={"large"} onSelect={() => setTime(true)} disabled={!date}/>
 
             <Typography.Title>Select Time of Booking</Typography.Title>
-            <WidthSelect defaultValue={"default"} onChange={(value) => {
-                console.log(value);
-                setRange(!(value === "default"))
-            }} options={[
+            <WidthSelect onChange={value => console.log(value)} options={[
                 {value: '30', label: '30 Minutes'},
                 {value: '60', label: '1 Hour'},
                 {value: '90', label: '1.5 Hours'},
                 {value: '120', label: '2 Hours'},
                 {value: '150', label: '2.5 Hours'},
                 {value: '180', label: '3 Hours'},
-            ]} size={"large"} disabled={(!(date && time))}/>
+            ]} size={"large"} onSelect={() => setRange(true)} disabled={(!(date && time))}/>
 
             <Typography.Title>Select Room</Typography.Title>
-            <WidthSelect defaultValue={"default"} onChange={(value) => {
-                console.log(value);
-                setRoom(!(value === "default"));
-                checkValues();
-            }} options={[
+            <WidthSelect onChange={value => console.log(value)} options={[
                 {value: '304', label: '304'},
-            ]} size={"large"} disabled={!(date && time && range)}/>
+            ]} size={"large"} onSelect={() => {setRoom(true); checkValues()}} disabled={!(date && time && range)}/>
+
 
             <div>{buttonState?.show && <MainButton {...buttonState} />}</div>
 
