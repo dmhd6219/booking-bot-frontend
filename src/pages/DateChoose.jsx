f0f7ecef5199196fa58b830742147f1688577f0d
Iceflue2 <svyatoslavsvyatkin@yandex.ru>
2023-06-20 20:16:50 +0300

    experimenting with size of selectors

--- src/pages/DateChoose.jsx
+++ src/pages/DateChoose.jsx
@@ -63,7 +63,7 @@ const DateChoose = () => {
                     <Select onChange={value => {
                         setDay(value);
                         console.log(day)
-                    }} size={"large"}>
+                    }} size={"large"} style={{ width: 200 }}>
                         <Select.Option value="20.06.2022">20.06.2022</Select.Option>
                         <Select.Option value="21.06.2022">21.06.2022</Select.Option>
                         <Select.Option value="22.06.2022">22.06.2022</Select.Option>
