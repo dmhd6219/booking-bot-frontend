b5fcb26da80054791a90e74fc767879e3f7d0272
Iceflue2 <svyatoslavsvyatkin@yandex.ru>
2023-06-22 13:45:47 +0300

    antd-mobile debug

--- src/pages/CascadePickerDemo.jsx
+++ src/pages/CascadePickerDemo.jsx
@@ -7,8 +7,106 @@ function CascadePickerDemo() {
 
     const [options,] = useState([
         {
+            label: '00',
+            value: '00',
+            children: [
+                {
+                    label: '00',
+                    value: '00',
+                },
+                {
+                    label: '30',
+                    value: '30',
+                }
+            ],
+        },
+        {
             label: '01',
             value: '01',
+            children: [
+                {
+                    label: '00',
+                    value: '00',
+                },
+                {
+                    label: '30',
+                    value: '30',
+                }
+            ],
+        },
+        {
+            label: '02',
+            value: '02',
+            children: [
+                {
+                    label: '00',
+                    value: '00',
+                },
+                {
+                    label: '30',
+                    value: '30',
+                }
+            ],
+        },
+        {
+            label: '03',
+            value: '03',
+            children: [
+                {
+                    label: '00',
+                    value: '00',
+                },
+                {
+                    label: '30',
+                    value: '30',
+                }
+            ],
+        },
+        {
+            label: '04',
+            value: '04',
+            children: [
+                {
+                    label: '00',
+                    value: '00',
+                },
+                {
+                    label: '30',
+                    value: '30',
+                }
+            ],
+        },
+        {
+            label: '05',
+            value: '05',
+            children: [
+                {
+                    label: '00',
+                    value: '00',
+                },
+                {
+                    label: '30',
+                    value: '30',
+                }
+            ],
+        },
+        {
+            label: '06',
+            value: '06',
+            children: [
+                {
+                    label: '00',
+                    value: '00',
+                },
+                {
+                    label: '30',
+                    value: '30',
+                }
+            ],
+        },
+        {
+            label: '19',
+            value: '19',
             children: [
                 {
                     label: '00',
@@ -34,6 +132,48 @@ function CascadePickerDemo() {
                 }
             ],
         },
+        {
+            label: '21',
+            value: '21',
+            children: [
+                {
+                    label: '00',
+                    value: '00',
+                },
+                {
+                    label: '30',
+                    value: '30',
+                }
+            ],
+        },
+        {
+            label: '22',
+            value: '22',
+            children: [
+                {
+                    label: '00',
+                    value: '00',
+                },
+                {
+                    label: '30',
+                    value: '30',
+                }
+            ],
+        },
+        {
+            label: '23',
+            value: '23',
+            children: [
+                {
+                    label: '00',
+                    value: '00',
+                },
+                {
+                    label: '30',
+                    value: '30',
+                }
+            ],
+        },
     ])
 
     return (
@@ -45,7 +185,7 @@ function CascadePickerDemo() {
                 }}
                 color="primary"
             >
-                WHAT
+                Choose Time
             </Button>
             <CascadePicker
                 title='Picker'
