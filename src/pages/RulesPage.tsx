a408587f7ac195ef95668d31340cec2998e6b7a1
Iceflue2 <svyatoslavsvyatkin@yandex.ru>
2023-06-28 12:26:37 +0300

    fix rules header deployment

--- src/pages/RulesPage.tsx
+++ src/pages/RulesPage.tsx
@@ -9,27 +9,36 @@ display: flex;
   justify-content: space-around;
   align-items: center;
 `
+
+const CenteredTitle = styled(Typography.Title)`
+  text-align: center;
+`
+
+const CardWithPadding = styled(Card)`
+  margin-top:5px;
+  margin-bottom: 5px;
+`
 export default function RulesPage() {
     return (
         <div>
-            <Typography.Title>Rules</Typography.Title>
+            <CenteredTitle>Rules</CenteredTitle>
             <CenteredSpace>
-                <Card title="Booking time" style={{ width: 300 }}>
+                <CardWithPadding title="Booking time" style={{ width: 300 }}>
                     <p>Booking is not allowed anytime because of university classes</p>
                     <p>Usually you cannot book a room from 7:00 to 19:00</p>
-                </Card>
-                <Card title="Available rooms" extra={<a href="/map">See rooms</a>} style={{ width: 300 }}>
+                </CardWithPadding>
+                <CardWithPadding title="Available rooms" extra={<a href="/map">See rooms</a>} style={{ width: 300 }}>
                     <p>You can book only rooms with Yellow access</p>
                     <p>You can see available for booking rooms by clicking "See rooms"</p>
-                </Card>
-                <Card title="Booking time" style={{ width: 300 }}>
+                </CardWithPadding>
+                <CardWithPadding title="Booking time" style={{ width: 300 }}>
                     <p>You can book one room not more than 3 horus per day</p>
                     <p>In case you need more, other people can book it again</p>
-                </Card>
-                <Card title="Please respect" style={{ width: 300 }}>
+                </CardWithPadding>
+                <CardWithPadding title="Please respect" style={{ width: 300 }}>
                     <p>Your booking can be removed by administrators for some important events or classes</p>
                     <p>You will be informed in this case</p>
-                </Card>
+                </CardWithPadding>
             </CenteredSpace>
         </div>
     )
