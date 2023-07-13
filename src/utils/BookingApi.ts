d11a26fde636baf2a2acbdfded86e777b4ef0f15
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2023-07-13 10:25:42 +0300

    getting duration process debug #3

--- src/utils/BookingApi.ts
+++ src/utils/BookingApi.ts
@@ -147,8 +147,8 @@ export async function getDurationByTime(date: Date, step: number): Promise<numbe
             if (!(freeRooms.length === 0)) {
                 // @ts-ignore
                 let diff: number = Math.abs((temp - startDate) / (1000 * 60));
-                console.log(`temp - ${temp.toISOString()}; ${temp.getHours()}; ${temp.getHours() < 7}; ${temp.getHours() > 18}`)
-                if (diff > 0 && temp.getHours() > 18 && temp.getHours() < 7) {
+                console.log(`temp : ${temp.toISOString()}; ${temp.getHours()}; < 7 : ${temp.getHours() < 7}; > 18 : ${temp.getHours() > 18}`)
+                if (diff > 0 && (temp.getUTCHours() > 18 || temp.getUTCHours() < 7)) {
                     freeMinutes.push(diff);
                 }
             }
