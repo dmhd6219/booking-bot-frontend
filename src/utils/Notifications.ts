0cece3e9887904f172776132cfaa9d9d7af5d4a5
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2023-07-15 20:55:28 +0300

    changed notifications method

--- src/utils/Notifications.ts
+++ src/utils/Notifications.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
-const url = `${process.env.REACT_APP_BOT_URL}`
+const url = `${process.env.REACT_APP_BOT_URL}/ntfctn`
 
 export function sendNotification() {
-    axios.get(url).then(r => {
-        console.log(r)
+    axios.post(url).then(r => {
+        console.log(r);
+        console.log(r.status);
+        console.log(r.data);
     })
 }
