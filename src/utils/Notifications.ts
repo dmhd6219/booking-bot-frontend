ffab0fcfea202d94dfd02c22c1b772594d533a87
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2023-07-15 18:16:51 +0300

    notifications debug #4

--- src/utils/Notifications.ts
+++ src/utils/Notifications.ts
@@ -2,6 +2,9 @@ const uri = `${process.env.REACT_APP_NOTIFICATIONS_URL}`
 
 export function sendNotification() {
     let connection = new WebSocket(uri);
+    connection.onopen = () => {
+        connection.send('asda');
+    }
 
-    connection.send('asda');
+
 }
